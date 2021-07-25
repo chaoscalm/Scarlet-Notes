033b993ccbf3abeb284061f7b8e834099702e3f3
Fs00 <francescosaltori@gmail.com>
2021-07-25 21:02:08 +0200

    Remove unnecessary IFolderContainer interface

--- base/src/main/java/com/maubis/scarlet/base/backup/data/ExportableFolder.kt
+++ base/src/main/java/com/maubis/scarlet/base/backup/data/ExportableFolder.kt
@@ -1,6 +1,5 @@
 package com.maubis.scarlet.base.backup.data
 
-import com.maubis.scarlet.base.core.folder.IFolderContainer
 import com.maubis.scarlet.base.database.entities.Folder
 import java.io.Serializable
 import java.util.*
@@ -11,12 +10,7 @@ class ExportableFolder(
   val timestamp: Long,
   val updateTimestamp: Long,
   val color: Int
-) : Serializable, IFolderContainer {
-  override fun timestamp(): Long = timestamp
-  override fun updateTimestamp(): Long = updateTimestamp
-  override fun color(): Int = color
-  override fun title(): String = title
-  override fun uuid(): String = uuid
+) : Serializable {
 
   constructor(folder: Folder) : this(
           folder.uuid,
