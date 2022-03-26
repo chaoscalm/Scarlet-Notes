a6c1e4af86514c3eff06979fe64e72739c84870d
Fs00 <francescosaltori@gmail.com>
2022-03-26 14:24:36 +0100

    Remove info about previous app developers

--- base/src/main/java/com/maubis/scarlet/base/settings/AboutUsBottomSheet.kt
+++ base/src/main/java/com/maubis/scarlet/base/settings/AboutUsBottomSheet.kt
@@ -18,7 +18,6 @@ class AboutUsBottomSheet : LithoBottomSheet() {
 
   override fun getComponent(componentContext: ComponentContext, dialog: Dialog): Component {
     val appName = getString(R.string.app_name)
-    val aboutUsDetails = getString(R.string.about_page_about_us_details, appName)
     val aboutAppDetails = getString(R.string.about_page_description, appName)
 
     val component = Column.create(componentContext)
@@ -29,20 +28,6 @@ class AboutUsBottomSheet : LithoBottomSheet() {
         getLithoBottomSheetTitle(componentContext)
           .textRes(R.string.home_option_about_page)
           .marginDip(YogaEdge.HORIZONTAL, 0f))
-      .child(
-        Text.create(componentContext)
-          .typeface(appTypeface.text())
-          .textSizeRes(R.dimen.font_size_large)
-          .marginDip(YogaEdge.BOTTOM, 16f)
-          .text(aboutUsDetails)
-          .textColor(appTheme.get(ThemeColorType.TERTIARY_TEXT)))
-      .child(
-        Text.create(componentContext)
-          .textSizeRes(R.dimen.font_size_xlarge)
-          .marginDip(YogaEdge.BOTTOM, 4f)
-          .textRes(R.string.about_page_about_app)
-          .typeface(appTypeface.title())
-          .textColor(appTheme.get(ThemeColorType.SECTION_HEADER)))
       .child(
         Text.create(componentContext)
           .typeface(appTypeface.text())
