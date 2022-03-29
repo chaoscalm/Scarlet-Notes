3af57f2e5944d02badada1104165282a26e79843
Fs00 <francescosaltori@gmail.com>
2022-03-29 15:46:20 +0200

    Do not subclass FileProvider anymore
    Newer versions of EasyImage extend FileProvider by themselves

--- base/src/main/java/com/maubis/scarlet/base/backup/support/ScarletFileProvider.kt
+++
@@ -1,6 +0,0 @@
-package com.maubis.scarlet.base.backup.support
-
-import androidx.core.content.FileProvider
-
-// This subclass is needed to avoid conflicts with FileProvider provided by EasyImage library
-class ScarletFileProvider : FileProvider()
