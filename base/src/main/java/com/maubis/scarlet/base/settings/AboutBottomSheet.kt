c7940c9e7f48c3fa8a6e18dc60fc8033ca8e12dc
Fs00 <francescosaltori@gmail.com>
2022-05-21 16:47:26 +0200

    Add button to open full license in About screen

--- base/src/main/java/com/maubis/scarlet/base/settings/AboutBottomSheet.kt
+++ base/src/main/java/com/maubis/scarlet/base/settings/AboutBottomSheet.kt
@@ -1,6 +1,10 @@
 package com.maubis.scarlet.base.settings
 
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
 import com.facebook.litho.Column
 import com.facebook.litho.Component
 import com.facebook.litho.ComponentContext
@@ -12,6 +16,7 @@ import com.maubis.scarlet.base.ScarletApp.Companion.appTheme
 import com.maubis.scarlet.base.ScarletApp.Companion.appTypeface
 import com.maubis.scarlet.base.common.sheets.LithoBottomSheet
 import com.maubis.scarlet.base.common.sheets.getLithoBottomSheetTitle
+import com.maubis.scarlet.base.common.specs.BottomSheetBar
 import com.maubis.scarlet.base.common.ui.ThemeColorType
 
 class AboutBottomSheet : LithoBottomSheet() {
@@ -62,6 +67,21 @@ class AboutBottomSheet : LithoBottomSheet() {
           .marginDip(YogaEdge.BOTTOM, 16f)
           .textRes(R.string.about_page_license_description)
           .textColor(appTheme.get(ThemeColorType.TERTIARY_TEXT)))
+      .child(
+        BottomSheetBar.create(componentContext)
+          .primaryActionRes(R.string.about_page_license_read_full)
+          .onPrimaryClick {
+            tryOpenInBrowser("https://github.com/Fs00/Scarlet-Notes/blob/master/LICENSE")
+          })
     return component.build()
+  }
+
+  private fun tryOpenInBrowser(url: String) {
+    val licenseIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    try {
+      startActivity(licenseIntent)
+    } catch (_: ActivityNotFoundException) {
+      Toast.makeText(context, R.string.web_browser_missing, Toast.LENGTH_SHORT).show()
+    }
   }
 }
